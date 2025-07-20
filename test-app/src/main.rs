210c483470d5bc89f6a8e87181133ee80c56bbfd
Maximb <sprayrules@gmail.com>
2025-07-20 21:35:31 +0300

    update

--- test-app/src/main.rs
+++ test-app/src/main.rs
@@ -806,6 +806,7 @@ async fn run() {
                                         color_attachments: &[
                                             Some(wgpu::RenderPassColorAttachment {
                                                 view,
+                                                depth_slice: None,
                                                 resolve_target: None,
                                                 ops: wgpu::Operations {
                                                     load: wgpu::LoadOp::Clear(
